9bb4fd8b9bb595732b0a3d22d9d9b8172c2f83fe
mackliu <mackliu@gmail.com>
2022-07-11 16:33:02 +0800

    額外補充，不做也可以，別花太多時間在這

--- back.php
+++ back.php
@@ -1,6 +1,9 @@
 ﻿<?php
 $do=$_GET['do']??'title';
 include "base.php";
+if(!isset($_SESSION['login'])){
+    to("index.php");
+}
 $DB=new DB($do);
 ?>
 <!DOCTYPE html
@@ -79,7 +82,7 @@ $DB=new DB($do);
                             <td style="width:70%;font-weight:800; border:#333 1px solid; border-radius:3px;"
                                 class="cent"><a href="?do=admin" style="color:#000; text-decoration:none;">後台管理區</a>
                             </td>
-                            <td><button onclick="location.href='index.php?do=login'"
+                            <td><button onclick="location.href='./api/logout.php'"
                                     style="width:99%; margin-right:2px; height:50px;">管理登出</button></td>
                         </tr>
                     </tbody>

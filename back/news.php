60c0086d9dc15f761d0d4b1fd9474c21291a4971
mackliu <mackliu@gmail.com>
2022-07-11 10:44:45 +0800

    增加後台列表分頁功能

--- back/news.php
+++ back/news.php
@@ -12,7 +12,13 @@
                 </tr>
                 <?php 
                     
-                    $rows=$DB->all();
+                    $all=$DB->math('count',"id");
+                    $div=4;
+                    $pages=ceil($all/$div);
+                    $now=$_GET['p']??1;
+                    $start=($now-1)*$div;
+                    //select * from table limit 0,3   
+                    $rows=$DB->all(" limit $start,$div");
                     foreach($rows as $row){
                 ?>
                 <tr >
@@ -33,6 +39,26 @@
                 ?>
             </tbody>
         </table>
+        <div class="cent">
+
+            <?php
+                if(($now-1) > 0){
+                    $p=$now-1;
+                    echo "<a href='?do={$Str->table}&p=$p'> < </a>";
+                }
+                for($i=1;$i<=$pages;$i++){
+                    $fontsize=($now==$i)?'1.5rem':'';
+                    echo "<a href='?do={$Str->table}&p=$i' style='font-size:$fontsize'> ";
+                    echo $i;
+                    echo " </a>";
+                }
+                if(($now+1) <= $pages){
+                    $p=$now+1;
+                    echo "<a href='?do={$Str->table}&p=$p'> > </a>";
+                }
+            ?>
+            
+        </div>        
         <table style="margin-top:40px; width:70%;">
             <tbody>
                 <tr>

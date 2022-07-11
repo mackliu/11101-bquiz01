f0b436ef8289028dc876cbe61e3c2e69d2a01f84
mackliu <mackliu@gmail.com>
2022-07-11 11:53:51 +0800

    前台增加動態文字顯示及動畫輪播內容

--- back.php
+++ back.php
@@ -79,7 +79,7 @@ $DB=new DB($do);
                             <td style="width:70%;font-weight:800; border:#333 1px solid; border-radius:3px;"
                                 class="cent"><a href="?do=admin" style="color:#000; text-decoration:none;">後台管理區</a>
                             </td>
-                            <td><button onclick="document.cookie='user=';location.replace('?')"
+                            <td><button onclick="location.href='index.php?do=login'"
                                     style="width:99%; margin-right:2px; height:50px;">管理登出</button></td>
                         </tr>
                     </tbody>
